705af0aa57a378a869d2d309620748e49098cd8a
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-08-15 19:42:45 +0200

    Add files via upload

--- Learning GUI/swing/panels/src/Main.java
+++ Learning GUI/swing/panels/src/Main.java
@@ -1,5 +1,18 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Color;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame("Panels - course");
+        JPanel panel = new JPanel();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setSize(1000, 500);
+        frame.setVisible(true);
+
+        panel.setBackground(new Color(0xcc6600));
+        panel.setBounds(0,0,250,250);
+        frame.add(panel);
     }
 }
