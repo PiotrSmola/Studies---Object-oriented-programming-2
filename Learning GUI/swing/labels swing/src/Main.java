f517f62867f8643e977bc6ccc1f23e6b6f7c6998
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-08-15 18:59:32 +0200

    Add files via upload

--- Learning GUI/swing/labels swing/src/Main.java
+++ Learning GUI/swing/labels swing/src/Main.java
@@ -1,15 +1,24 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         JLabel label = new JLabel("New fancy and creative label");
+        Border borders = BorderFactory.createEtchedBorder(Color.red, new Color(0xcc6600));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Labels");
         frame.setSize(700, 500);
         frame.setVisible(true);
         frame.add(label);
         label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setForeground(new Color(0x7F00FF));
+        label.setFont(new Font("sylfaen", Font.BOLD,20));
+        label.setBackground(Color.black);
+        label.setOpaque(true);
+        label.setBorder(borders);
+
 
 
     }
