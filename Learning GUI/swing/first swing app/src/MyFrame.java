461adfaf5d6c8f98e65ec9fbacbdf4ee296b0c39
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-08-17 19:31:07 +0200

    Update MyFrame.java

--- Learning GUI/swing/first swing app/src/MyFrame.java
+++ Learning GUI/swing/first swing app/src/MyFrame.java
@@ -3,12 +3,11 @@ import java.awt.*;
 
 public class MyFrame extends JFrame {
     MyFrame() {
-        JFrame frame = new JFrame();
-        frame.setTitle("First swing app - course");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setSize(500, 350);
-        frame.setVisible(true);
-        frame.getContentPane().setBackground(Color.blue);
+        this.setTitle("First swing app - course");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setSize(500, 350);
+        this.setVisible(true);
+        this.getContentPane().setBackground(Color.blue);
     }
 }
