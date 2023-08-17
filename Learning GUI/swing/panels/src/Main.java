20194bdaabb83c449d00819ae0862eb49383766d
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-08-17 19:21:59 +0200

    Add files via upload

--- Learning GUI/swing/panels/src/Main.java
+++ Learning GUI/swing/panels/src/Main.java
@@ -1,5 +1,5 @@
 import javax.swing.*;
-import java.awt.Color;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,6 +7,7 @@ public class Main {
         JPanel panel = new JPanel();
         JPanel panel2 = new JPanel();
         JLabel label = new JLabel();
+        JLabel label2 = new JLabel();
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
@@ -16,15 +17,20 @@ public class Main {
         panel.setBackground(new Color(0xcc6600));
         panel.setBounds(0,0,250,250);
         panel.setBorder(BorderFactory.createMatteBorder(2, 2, 0, 0, Color.BLACK));
+        panel.setLayout(new BorderLayout());
         frame.add(panel);
+        label.setText("Hello! It's me, first panel");
+        label.setHorizontalAlignment(JLabel.CENTER);
+        panel.add(label);
 
         panel2.setBackground(new Color(0,255,0));
         panel2.setBounds(250,250,250,250);
         panel2.setBorder(BorderFactory.createMatteBorder(0, 0, 2, 2, Color.BLACK));
+        panel2.setLayout(new BorderLayout());
         frame.add(panel2);
-
-        label.setText("Hello! It's me, first panel");
-        panel.add(label);
-        label.setHorizontalTextPosition(JLabel.CENTER);
+        label2.setText("Hello! It's me, second panel");
+        panel2.add(label2);
+        label2.setVerticalAlignment(JLabel.BOTTOM);
+        label2.setHorizontalAlignment(JLabel.RIGHT);
     }
 }
