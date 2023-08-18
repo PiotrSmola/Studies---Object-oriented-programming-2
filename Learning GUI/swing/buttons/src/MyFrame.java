a0ef80f6cec3d1360936921f313da791918af50f
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-08-18 14:50:57 +0200

    Add files via upload

--- Learning GUI/swing/buttons/src/MyFrame.java
+++ Learning GUI/swing/buttons/src/MyFrame.java
@@ -1,5 +1,5 @@
-import javax.swing.JButton;
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -7,9 +7,15 @@ public class MyFrame extends JFrame /*implements ActionListener*/ {
     JButton button;
     MyFrame(){
         button = new JButton("Click me, if you can");
-        button.setBounds(400,250,200,50);
+        button.setBounds(400,250,300,50);
         //button.addActionListener(this);
         button.addActionListener(e -> System.out.println("How you dare!"));
+        button.setFocusable(false);
+        button.setFont(new Font("Comic Sans", Font.ITALIC, 20));
+        button.setBackground(Color.BLUE);
+        button.setForeground(Color.RED);
+        button.setBorder(BorderFactory.createLineBorder(Color.BLACK,2));
+        //button.setEnabled(false);
 
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(null);
