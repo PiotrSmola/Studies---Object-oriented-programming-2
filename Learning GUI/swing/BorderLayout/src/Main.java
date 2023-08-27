c5feda36d2832ed0c62fc5e909f81b99ba854adc
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-08-27 17:13:44 +0200

    Add files via upload

--- Learning GUI/swing/BorderLayout/src/Main.java
+++ Learning GUI/swing/BorderLayout/src/Main.java
@@ -1,11 +1,13 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Border Layout - course");
-        frame.setVisible(true);
         frame.setSize(1000,700);
+        frame.setLayout(new BorderLayout(5, 5));
+        frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
@@ -26,6 +28,34 @@ public class Main {
         panel3.setPreferredSize(new Dimension(200, 100));
         panel4.setPreferredSize(new Dimension(100, 200));
         panel5.setPreferredSize(new Dimension(100, 100));
+
+        /* ----------------- sub panels ----------------- */
+
+        panel5.setLayout(new BorderLayout());
+
+        JPanel panel6 = new JPanel();
+        JPanel panel7 = new JPanel();
+        JPanel panel8 = new JPanel();
+        JPanel panel9 = new JPanel();
+        JPanel panel10 = new JPanel();
+
+        panel6.setBackground(Color.pink);
+        panel7.setBackground(Color.cyan);
+        panel8.setBackground(Color.yellow);
+        panel9.setBackground(Color.orange);
+        panel10.setBackground(Color.white);
+
+        panel6.setPreferredSize(new Dimension(50, 50));
+        panel7.setPreferredSize(new Dimension(50, 50));
+        panel8.setPreferredSize(new Dimension(50, 50));
+        panel9.setPreferredSize(new Dimension(50, 50));
+        panel10.setPreferredSize(new Dimension(50, 50));
+        panel5.add(panel6, BorderLayout.NORTH);
+        panel5.add(panel7, BorderLayout.EAST);
+        panel5.add(panel8, BorderLayout.SOUTH);
+        panel5.add(panel9, BorderLayout.WEST);
+        panel5.add(panel10, BorderLayout.CENTER);
+        /* ---------------------------------------- */
 
         frame.add(panel1, BorderLayout.NORTH);
         frame.add(panel2, BorderLayout.EAST);
