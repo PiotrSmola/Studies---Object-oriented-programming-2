aef95df63d5d62799c238c483fd0d6b114afbee1
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-08-27 18:09:30 +0200

    Add files via upload

--- Learning GUI/swing/BorderLayout/src/Main.java
+++ Learning GUI/swing/BorderLayout/src/Main.java
@@ -1,6 +1,8 @@
-import javax.swing.*;
-import javax.swing.border.Border;
-import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 
 public class Main {
     public static void main(String[] args) {
