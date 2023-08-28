ee7efbdd623d4d093fa70d57393b686fb573a034
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-08-28 17:09:31 +0200

    Add files via upload

---
+++ Learning GUI/swing/GridLayout/src/Main.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("GridLayouts - course");
+        frame.setSize(1000, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout(2,3));
+
+
+        frame.add(new JButton("First button"));
+        frame.add(new JButton("Second button"));
+        frame.add(new JButton("Third button"));
+        frame.add(new JButton("Fourth button"));
+        frame.add(new JButton("Fifth button"));
+        frame.add(new JButton("Sixth button"));
+
+        frame.setVisible(true);
+
+
+
+    }
+}
