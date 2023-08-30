7f2cfc7b7d596a38cf283cac7f0ec2765204d4ad
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-08-30 18:18:02 +0200

    Add files via upload

--- Learning GUI/swing/JLayeredPane/src/Main.java
+++ Learning GUI/swing/JLayeredPane/src/Main.java
@@ -4,10 +4,36 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("JLayeredPane - course");
-        JLayeredPane layeredPane = new JLayeredPane();
         frame.setSize(new Dimension(1000,500));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
         frame.setVisible(true);
+///////
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setBounds(0,0,1000,500);
+        frame.add(layeredPane);
+///////
+        JLabel label = new JLabel();
+        label.setOpaque(true);
+        label.setBackground(Color.red);
+        label.setBounds(100,100,200,200);
+        layeredPane.add(label);
+
+        JLabel label2 = new JLabel();
+        label2.setOpaque(true);
+        label2.setBackground(Color.yellow);
+        label2.setBounds(150,150,200,200);
+        layeredPane.add(label2);
+
+        JLabel label3 = new JLabel();
+        label3.setOpaque(true);
+        label3.setBackground(Color.green);
+        label3.setBounds(200,200,200,200);
+        layeredPane.add(label3);
+        /*better visual option
+        layeredPane.add(label3);
+        layeredPane.add(label);
+        layeredPane.add(label2);*/
+
     }
 }
