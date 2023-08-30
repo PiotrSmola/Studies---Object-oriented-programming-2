4789a9e9d6860feedf141733d5c7dfc8d331366a
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-08-30 19:03:59 +0200

    Add files via upload

---
+++ Learning GUI/swing/New Window/src/LaunchPage.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class LaunchPage {
+    JFrame frame = new JFrame();
+    JButton button = new JButton();
+    LaunchPage(){
+        frame.setSize(1000,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+    }
+}
