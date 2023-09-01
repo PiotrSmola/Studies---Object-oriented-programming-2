d5d1094e17d72515d37b0dbcea494de89a7aeeca
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-09-01 16:18:46 +0200

    Add files via upload

--- Learning GUI/swing/JOptionPane/src/Main.java
+++ Learning GUI/swing/JOptionPane/src/Main.java
@@ -1,5 +1,21 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JOptionPane.showMessageDialog(null,"Some extremely important information","Fancy Title",JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null,"Some extremely important information","Fancy Title",JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null,"Some extremely important information","Fancy Title",JOptionPane.QUESTION_MESSAGE);
+        JOptionPane.showMessageDialog(null,"Some extremely important information","Fancy Title",JOptionPane.WARNING_MESSAGE);
+        JOptionPane.showMessageDialog(null,"Some extremely important information","Fancy Title",JOptionPane.ERROR_MESSAGE);
+
+        JOptionPane.showConfirmDialog(null, "Yes or No?", "Confirmation", JOptionPane.YES_NO_OPTION);
+        System.out.println(JOptionPane.showConfirmDialog(null, "Yes or No?", "Confirmation", JOptionPane.YES_NO_OPTION));
+
+        int answer = JOptionPane.showConfirmDialog(null, "Yes or No or Cancel?", "Confirmation", JOptionPane.YES_NO_CANCEL_OPTION);
+        String age = JOptionPane.showInputDialog("How old are you?: ");
+        System.out.println(age);
+
+
+
     }
 }
