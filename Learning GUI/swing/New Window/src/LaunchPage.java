412d380af8b4e29d777e4e3177e691d10997de43
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-09-01 15:09:09 +0200

    Add files via upload

--- Learning GUI/swing/New Window/src/LaunchPage.java
+++ Learning GUI/swing/New Window/src/LaunchPage.java
@@ -1,13 +1,27 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LaunchPage {
+public class LaunchPage implements ActionListener {
     JFrame frame = new JFrame();
-    JButton button = new JButton();
+    JButton button = new JButton("Click me!");
     LaunchPage(){
         frame.setSize(1000,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
         frame.setVisible(true);
 
+        button.setBounds(200,100,150,50);
+        button.setFocusable(false);
+        button.addActionListener(this);
+        frame.add(button);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button) {
+            NewWindow newWindow = new NewWindow();
+        }
     }
 }
