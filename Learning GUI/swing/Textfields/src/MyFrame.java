468954eb14c2faeab59c0ed2a2611c6780282e92
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-09-03 16:50:19 +0200

    Add files via upload

---
+++ Learning GUI/swing/Textfields/src/MyFrame.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyFrame extends JFrame implements ActionListener {
+    JTextField textField = new JTextField();
+    JButton button = new JButton("Submit");
+
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Textfields - course");
+        this.setLayout(new FlowLayout());
+
+        textField.setPreferredSize(new Dimension(250, 50));
+        textField.setFont(new Font("Tahoma", Font.PLAIN,20));
+        textField.setForeground(Color.BLUE);
+        textField.setBackground(Color.BLACK);
+        textField.setCaretColor(Color.white);
+        textField.setText("Enter your name");
+        this.add(textField);
+
+        button.addActionListener(this);
+        this.add(button);
+
+        this.pack();
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button) {
+            System.out.println("Welcome Mr./Ms. " + textField.getText());
+        }
+    }
+}
