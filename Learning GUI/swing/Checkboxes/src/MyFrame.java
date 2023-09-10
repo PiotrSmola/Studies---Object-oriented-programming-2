3633a48ac6c3bfce93c3b0e25bc5fd0d094b6951
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-09-10 20:33:33 +0200

    Add files via upload

--- Learning GUI/swing/Checkboxes/src/MyFrame.java
+++ Learning GUI/swing/Checkboxes/src/MyFrame.java
@@ -1,5 +1,8 @@
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,10 +16,13 @@ public class MyFrame extends JFrame implements ActionListener {
         button.setText("Submit");
         button.addActionListener(this);
 
+        //also can change default icon of checkbox to a custom icon
+
         checkBox = new JCheckBox("First option", true);
         checkBox.setFocusable(false);
         checkBox.setFont(new Font("Calibri", Font.PLAIN,24));
         this.add(checkBox);
+        this.add(button);
 
         this.pack();
         this.setVisible(true);
@@ -25,7 +31,7 @@ public class MyFrame extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == button) {
-
+            System.out.println(checkBox.isSelected());
         }
     }
 }
