f06134d5b37c42dc3f6a98f3ea8d09d09f3d676f
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-10-23 08:49:31 +0200

    adding a temperature conversion function

--- JAVA GUI/Lab01/src/Temperatura/konwersjaTemperatury.java
+++ JAVA GUI/Lab01/src/Temperatura/konwersjaTemperatury.java
@@ -1,10 +1,12 @@
 package Temperatura;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class konwersjaTemperatury extends JFrame {
     private JPanel panel1;
-    private JTextField textField1;
-    private JTextField textField2;
+    private JTextField JCelsjusz;
+    private JTextField JFahrenheit;
     private JButton konwertujButton;
     private JLabel JHeader;
 
@@ -17,5 +19,17 @@ public class konwersjaTemperatury extends JFrame {
         super("Konwersja temperatury");
         this.setContentPane(panel1);
         this.pack();
+        konwertujButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    double celsjusz = Double.parseDouble(JCelsjusz.getText());
+                    double fahrenheit = (celsjusz * 1.8) + 32;
+                    JFahrenheit.setText(String.valueOf(fahrenheit));
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null, "Wprowadź liczbę!");
+                }
+            }
+        });
     }
 }
