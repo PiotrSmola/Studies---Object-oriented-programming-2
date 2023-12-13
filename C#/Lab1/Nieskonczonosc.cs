1c4c6d563a841d058f007c9be7e31ca7f9f1bc55
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-12-13 19:26:19 +0100

    Add files via upload

---
+++ C#/Lab1/Nieskonczonosc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.TASK6
+{
+    public class Nieskonczonosc
+    {
+        public static void RunNieskonczonosc()
+        {
+            while (true)
+            {
+                Console.WriteLine("Wprowadź liczbę całkowitą. W przypadku podania liczby ujemniej program kończy swe działanie");
+                int liczba = int.Parse(Console.ReadLine());
+
+                if (liczba < 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Wprowadzona liczba to: " + liczba);
+            }
+            Console.WriteLine("Wprowadzono liczbę ujemną, koniec programu");
+        }
+    }
+
+}
