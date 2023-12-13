71977451894e47120c07db975e0d58a0c00dcbed
Piotr Smoła <128245165+PiotrSmola@users.noreply.github.com>
2023-12-13 19:23:23 +0100

    Add files via upload

---
+++ C#/Lab1/Wyswietlanie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.TASK5
+{
+    public class Wyswietlanie
+    {
+        public static void RunWyswietlanie()
+        {
+            for (int i = 20; i >= 0; i--)
+            {
+                if (i == 2 || i == 6 || i == 9 || i == 15 || i == 19)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+}
